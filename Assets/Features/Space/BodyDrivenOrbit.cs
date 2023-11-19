38d304a7899c0149297ea3c5b1ea1c2faa2957fb
Tiger Jove <tiger@jupiter.blue>
2023-11-19 19:57:40 +0100

    Orbit is nice.

--- Assets/Features/Space/BodyDrivenOrbit.cs
+++ Assets/Features/Space/BodyDrivenOrbit.cs
@@ -2,62 +2,66 @@ using Tiger.Math;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class BodyDrivenOrbit : MonoBehaviour
+namespace Features.Space
 {
-    [Header("These ain't your normal Kepler-ian Elements")] [SerializeField] [Tooltip("Length of one circumnavigation in unity Units")]
-    private float period = 1000;
-
-    [SerializeField] [Tooltip("Orbit auto-advances in this direction")]
-    private float2 intrinsicVelocity = 5;
-
-    [SerializeField] [Tooltip("Lowest point of orbit")]    
-    private float periapsis = 300;
-        
-    [SerializeField] [Tooltip("Highest point of orbit")]
-    private float apoapsis = 900;
-
-    [Header("Driving Behaviour")]
-    [SerializeField] [Tooltip("RigidBody to follow (e.g. Player)")]
-    private Rigidbody body;
-
-    [SerializeField] [Tooltip("Object to move and rotate underneath the body.")]
-    private Transform barycenter;
-
-    // The orbit tracks its own toroidal position, disregarding what other objects may use for their wrapping.
-    private float2 _toroidalPosition;
-    private void Update()
+    public class BodyDrivenOrbit : MonoBehaviour
     {
-        var dt = Time.deltaTime;
-        var velocity = body.velocity._xz();
-        _toroidalPosition += velocity * dt + intrinsicVelocity * dt;
-        print(_toroidalPosition);
-        print(mathex.eumod(_toroidalPosition, period));
-        _toroidalPosition = mathex.eumod(_toroidalPosition, period);
+        [Header("Body & Orbital velocities")] [SerializeField] [Tooltip("Orbit auto-advances in this direction")]    
+        private float2 intrinsicVelocity = 20;
+
+        [SerializeField] [Tooltip("scale velocity of body by this amount")]
+        private float bodyVelocityScale = 5;
+
+        [Header("These ain't your normal Kepler-ian Elements")][SerializeField] [Tooltip("Length of one circumnavigation in unity Units")]
+        private float period = 1000;
+
+        [SerializeField] [Tooltip("Lowest point of orbit")]    
+        private float periapsis = 300;
         
-        var location = math.smoothstep(0, period, math.length(_toroidalPosition));
-        var altitude = math.lerp(apoapsis, periapsis, location);
-        var position = Vector3.down * altitude;
+        [SerializeField] [Tooltip("Highest point of orbit")]
+        private float apoapsis = 900;
 
-        var orbitalDelta = velocity / altitude * dt;
-        var angularDelta = quaternion.Euler(orbitalDelta.x, 0, orbitalDelta.y);
-        var rotation = barycenter.localRotation * angularDelta;
-        
-        barycenter.SetLocalPositionAndRotation(position, rotation);
-    }
+        [Header("Driving Behaviour")]
+        [SerializeField] [Tooltip("Body to follow (e.g. Player)")]
+        private Rigidbody body;
 
-    private void OnValidate()
-    {
-        period = math.max(period, 1);
-        apoapsis = math.abs(apoapsis);
-        periapsis = math.clamp(periapsis, 0, apoapsis);
+        [SerializeField] [Tooltip("Object to move and rotate underneath the body.")]
+        private Transform barycenter;
 
-        if (barycenter)
+        // The orbit tracks its own toroidal position, disregarding what other objects may use for their wrapping.
+        private float2 _toroidalPosition;
+        private void LateUpdate()
         {
-            barycenter.localPosition = Vector3.down * apoapsis;
+            var dt = Time.deltaTime;
+            var velocity = intrinsicVelocity + body.velocity._xz() * bodyVelocityScale;
+            _toroidalPosition += velocity * dt;
+            _toroidalPosition = mathex.eumod(_toroidalPosition, period);
+        
+            var location = math.smoothstep(0, math.SQRT2 * period, math.length(_toroidalPosition));
+            var altitude = math.lerp(apoapsis, periapsis, location);
+            var position = Vector3.down * altitude;
+
+            var orbitalDelta = velocity / math.sqrt(altitude) * dt;
+            var angularDelta = Quaternion.Euler(orbitalDelta.y, 0, -orbitalDelta.x);
+            var rotation= angularDelta * barycenter.localRotation;
+        
+            barycenter.SetLocalPositionAndRotation(position, rotation);
         }
-        else
+
+        private void OnValidate()
         {
-            Debug.LogWarning("BodyDrivenOrbit: No barycenter (backdrop object) set");
+            period = math.max(period, 1);
+            apoapsis = math.abs(apoapsis);
+            periapsis = math.clamp(periapsis, 0, apoapsis);
+
+            if (barycenter)
+            {
+                barycenter.localPosition = Vector3.down * apoapsis;
+            }
+            else
+            {
+                Debug.LogWarning("BodyDrivenOrbit: No barycenter (backdrop object) set");
+            }
         }
     }
 }
