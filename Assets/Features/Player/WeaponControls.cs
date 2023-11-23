03e6ea49eb1aa77703aa4a5e199737ae9955b16f
Tiger Jove <tiger@jupiter.blue>
2023-11-23 14:23:01 +0100

    Found a way to statically initialize recoil tween, the 3.x library is really good.

--- Assets/Features/Player/WeaponControls.cs
+++ Assets/Features/Player/WeaponControls.cs
@@ -6,41 +6,38 @@ using Tiger.Util;
 using Tweens;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 namespace Features.Player
 {
     using Log = Loggers.Create<WeaponControls>;
 
     public class WeaponControls : MonoBehaviour, GameInputActions.IWeaponActions
     {
-        [Header("Effects")]        [SerializeField] private float cannonShake = 0.5f;
+        [Header("Effects")] [SerializeField] private float cannonShake = 0.5f;
         [SerializeField] private ParticleSystem[] cannonFX;
 
-        [SerializeField] [Header("Channels")]
-        private IntChannel ammoCountChannel;
+        [SerializeField] [Header("Channels")] private IntChannel ammoCountChannel;
 
-        [Header("Guns, Guns, Guns")] 
-        [SerializeField] private Rigidbody bulletPrefab;
+        [Header("Guns, Guns, Guns")] [SerializeField]
+        private Rigidbody bulletPrefab;
+
         [SerializeField] private float muzzleVelocity = 100;
         [SerializeField] private int clipSize = 4; // Our 1979 ancestor had 4 shots
         [SerializeField] private float reloadTime = 1;
         [SerializeField] private float cycleTime = 0.05f;
-
-    
+        
         private int _bullets;
         private float _reloadTimer;
         private float _cycleTimer;
-        
+
         private WorldBounds _world;
-
-        private Vector3Tween _recoil;
-
-
-        private bool canShoot => _cycleTimer <= 0 && _bullets > 0;
+        
+        #region Unity Events
 
         private void Awake()
         {
             Log.TagColor = Color.red;
-            
+
             _world = GetComponentInParent<WorldBounds>();
             if (!_world) Log.Error("No world bounds found in parent!", this);
         }
@@ -48,7 +45,7 @@ namespace Features.Player
         private void OnEnable()
         {
             _bullets = 0;
-            _reloadTimer = reloadTime; 
+            _reloadTimer = reloadTime;
         }
 
         private void Update()
@@ -63,12 +60,21 @@ namespace Features.Player
             }
         }
 
+        private void OnValidate()
+        {
+            if (gameObject.IsAsset()) return;
+            if (!bulletPrefab) Log.Error("No bullet prefab assigned!", this);
+        }
+
+        #endregion
+
+        #region Gun Logic
+
+        private bool canShoot => _cycleTimer <= 0 && _bullets > 0;
+
         public void OnFire(InputAction.CallbackContext context)
         {
-            if (canShoot && context.action.IsPressed())
-            {
-                Shoot();
-            }
+            if (canShoot && context.action.IsPressed()) Shoot();
         }
 
         private void Shoot()
@@ -80,32 +86,38 @@ namespace Features.Player
 
             var bullet = Instantiate(bulletPrefab, transform.position, transform.rotation, _world.transform);
             bullet.velocity = transform.forward * muzzleVelocity;
-            
+
             AddRecoilEffects();
         }
 
-        void AddRecoilEffects()
+        private void AddRecoilEffects()
         {
             var fx = cannonFX.Shift();
             fx.gameObject.SetActive(true);
             fx.Play();
             ScreenShake.Add(transform.position, cannonShake, 0);
 
-            _recoil = new Vector3Tween
-            {
-                from = -transform.forward * 0.5f,
-                to = Vector3.zero,
-                easeType = EaseType.SineInOut,
-                duration = 0.8f,
-                //gun rocks the entire ship ;) I know this is a feature envy smell but it's a fun effect.
-                onUpdate = (_, value) => transform.parent.position = value,
-            };
-            transform.parent.gameObject.AddTween(_recoil);
+            transform.gameObject.AddTween(_recoil);
         }
-        private void OnValidate()
+
+        private static void OnRecoilUpdate(TweenInstance<Transform, float> instance, float value)
         {
-            if (gameObject.IsAsset()) return;
-            if (!bulletPrefab) Log.Error("No bullet prefab assigned!", this);
+            var t = instance.target.transform.parent;
+            t.localPosition = -t.forward * value;
         }
+
+        private readonly FloatTween _recoil = new()
+        {
+            from = 0.5f,
+            to = 0,
+            easeType = EaseType.SineInOut,
+            duration = 0.8f,
+            //gun rocks the entire ship ;) I know this is a feature envy smell but it's a fun effect.
+            //TODO: occasionally conflicts with other tweens, for example the SpawnProcedure.
+            //(but these are exclusive to each other thanks to the game state, so no real life impact atm.)
+            onUpdate = OnRecoilUpdate,
+        };
+        
+        #endregion
     }
 }
