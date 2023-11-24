1e412d024a8129e9f15a2c9a2450f288f4fdf1ba
Tiger Jove <tiger@jupiter.blue>
2023-11-24 19:42:01 +0100

    Compile guard was at wrong nesting level

--- Assets/Features/Space/PlayerDrivenGravity.cs
+++ Assets/Features/Space/PlayerDrivenGravity.cs
@@ -7,21 +7,25 @@ namespace Features.Space
     public class PlayerDrivenGravity : DataChannelResponder<Vector3Channel, Vector3>
     {
         //Doesn't work with particles in Unity 2023. IKR?
-#if !UNITY_2023_1_OR_NEWER
         protected override void OnEvent(Vector3 data)
         {
-            Physics.gravity = data;  
+#if !UNITY_2023_1_OR_NEWER
+            Physics.gravity = data;
+#endif
         }
 
         private void Awake()
         {
-            Physics.gravity = Vector3.zero;  
+#if !UNITY_2023_1_OR_NEWER
+            Physics.gravity = Vector3.zero;
+#endif
         }
 
         private void OnDestroy()
         {
-            Physics.gravity = Vector3.zero;  
-        }
+#if !UNITY_2023_1_OR_NEWER
+            Physics.gravity = Vector3.zero;
 #endif
+        }
     }
 }
