6470a572cc6d41ffbf0d8a95f0d37bd2084c1b3d
Tiger Jove <tiger@jupiter.blue>
2023-11-24 20:01:06 +0100

    A much better way to place the audio listener.

--- Assets/CopyRotationAndLookPosition.cs
+++ Assets/CopyRotationAndLookPosition.cs
@@ -1,13 +1,16 @@
 using Tiger.Events;
 using UnityEngine;
 
-public class CopyRotation : MonoBehaviour, IHierarchicalUpdate
+public class CopyRotationAndLookPosition : MonoBehaviour, IHierarchicalUpdate
 {
     [SerializeField] private Transform source;
     
     void IHierarchicalUpdate.HierarchicUpdate(float deltaTime)
     {
         transform.rotation = source.rotation;
+        var plane = new Plane(Vector3.up, Vector3.zero);
+        var ray = new Ray(source.position, source.forward);
+        transform.position = plane.Raycast(ray, out var distance) ? ray.GetPoint(distance) : source.position;
     }
 
     private void OnValidate()
