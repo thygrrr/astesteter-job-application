10ac11cf7a6ec809705d4386b3a79c9b6524f33d
Tiger Jove <tiger@jupiter.blue>
2023-11-29 00:11:48 +0100

    end of day.

--- Packages/Tiger/Util/Constants/Colors.cs
+++ Packages/Tiger/Util/Constants/Colors.cs
@@ -11,5 +11,19 @@ namespace Tiger.Util.Constants
         public static readonly Color pink = new Color(1.0f, 0.5f, 0.6f, 1f);
         public static readonly Color sky = new Color(0.5f, 0.6f, 1.0f, 1f); 
         public static readonly Color coal = new Color(0.2f, 0.2f, 0.2f, 1f);
+
+        public static readonly Color coral = new Color32(255, 127, 80, 255);
+        public static readonly Color navy = new Color32(0, 0, 128, 255);
+        public static readonly Color lavender = new Color(0.9f, 0.7f, 1.0f, 1f);
+        public static readonly Color olive = new Color32(128, 128, 0, 255);
+        public static readonly Color teal = new Color(0.0f, 0.5f, 0.5f, 1f);
+        public static readonly Color maroon = new Color32(128, 0, 0, 255);
+
+        public static readonly Color turquoise = new Color32(64, 224, 208, 255);
+        public static readonly Color magenta = new Color(1.0f, 0.0f, 1.0f, 1f);
+        public static readonly Color sienna = new Color32(160, 82, 45, 255);
+        public static readonly Color plum = new Color(0.8f, 0.6f, 0.8f, 1f);
+        public static readonly Color indigo = new Color32(75, 0, 130, 255);
+        public static readonly Color lime = new Color32(0, 255, 0, 255);
     }
 }
